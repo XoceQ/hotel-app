fea3108f957e849017762ada3325b2250940ffdf
XoceQ <josefernandorevelo@gmail.com>
2024-09-30 18:16:36 -0500

    Initial commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import {BsFillSendFill, BsTelephoneOutbound} from "react-icons/bs";
+import {BiMessageDetail} from "react-icons/bi";
+
+const Footer = () => {
+    return (
+        <footer className='mt-16'>
+                <div className='container mx-auto px-4'>
+                    <Link href='/' className='font-black text-tertiary-dark'>
+                        Hotel App
+                    </Link>
+                    <h4 className='font-semibold text-[40px] py-6 '>Contact</h4>
+
+                    <div className='flex flex-wrap gap-16 items-center justify-between'>
+                        <div className='flex-1'>
+                            <p>123 Road</p>
+                            <div className='flex items-center py-4'>
+                                <BsFillSendFill/>
+                                <p className="ml-2">code with Kose</p>
+                            </div>
+                            <div className='flex items-center '>
+                                <BsTelephoneOutbound/>
+                                <p className="ml-2">000-000-00-00</p>
+                            </div>
+                            <div className='flex items-center py-4'>
+                                <BiMessageDetail/>
+                                <p className="ml-2">dcurbanxtreme@gmail.com</p>
+                            </div>
+                        </div>
+                        <div className="flex-1 md-text-right">
+                            <p className="pb-4">Our story</p>
+                            <p className="pb-4">Get in touch</p>
+                            <p className="pb-4">Our privacy commitment</p>
+                            <p className="pb-4">Terms of services</p>
+                            <p> Customer Asssitance</p>
+                        </div>
+                        <div className='flex-1 md:text-right'>
+                            <p className='pb-4'>Dining Experience</p>
+                            <p className='pb-4'>Wellness</p>
+                            <p className='pb-4'>Fitness</p>
+                            <p className='pb-4'>Sports</p>
+                            <p>Events</p>
+                        </div>
+                    </div>
+
+                </div>
+            <div className='bg-tertiary-light h-10 md:h-[70px] mt-16 w-full bottom-0 left-0'></div>
+        </footer>
+    );
+};
+export default Footer;
